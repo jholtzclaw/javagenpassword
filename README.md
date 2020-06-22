a0dc2fe1d39ac829891d787f94fc181ecc2fa094
Jena Holtzclaw <65194896+jholtzclaw@users.noreply.github.com>
2020-06-21 20:25:42 -0500

    moved index.html to main

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## Purpose
 The purpose of this was to create a password generator that could respond in multiple ways. It allows the user to create
 a random password using different criteria. The user can chose to use numbers, special characters, uppercase or lowercase letters. Once they select which of the criteria the password is generated. 
+
+## Built With
+ JavaScript
+
+ ## Website
+ https://jholtzclaw.github.io/js-parakeet/.
--- develop/script.js
+++ develop/script.js
@@ -1,3 +1,4 @@
+
  // Assignment code here
  var lowercaseChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' ];
  var uppercaseChar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' ];
@@ -9,22 +10,22 @@
  
  // Write password to the #password input
  function writePassword() {
+  
    // var password = generatePassword();
    var passwordLength = prompt("Please enter how long you want your password to be : Must be 8-128");
-   if (passwordLength.length < 8) {
+   if (passwordLength < 8) {
      alert("Your password must be at least 8 characters long")
    }
-   if (passwordLength.length > 128) {
+   if (passwordLength > 128) {
      alert("Your password must be less than 128 characters long")
    }
-   
  
    var passwordText = document.querySelector("#password");
  
    passwordText.value = password;
-  
  
  }
  
  // Add event listener to generate button
+
  generateBtn.addEventListener("click", writePassword);
