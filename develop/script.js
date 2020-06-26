7d16a97a69d843071058a90799ec6477a9f076a7
Jena Holtzclaw <65194896+jholtzclaw@users.noreply.github.com>
2020-06-26 15:12:02 -0500

    fixed var function

--- develop/script.js
+++ develop/script.js
@@ -1,10 +1,22 @@
+ // Get references to the #generate element
+ var generateBtn = document.querySelector("#generate");
 
- // Assignment code here
+var passLength;
+
+
+ // Arrays for letter, special characters, and numbers.
  var lowercaseChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' ];
  var uppercaseChar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' ];
  var numberChar = ['1','2','3','4','5','6','7','8','9','0' ];
  var specialChar= ['!','@','#','$','%','^','&','*' ];
  
+
+ // Variable Declaration
+ var confirmLowercaseChar;
+ var confirmUppercaseChar;
+ var confirmNumberChar;
+ var specialChar;
+
  // Get references to the #generate element
  var generateBtn = document.querySelector("#generate");
  
@@ -18,28 +30,29 @@
      var passwordLength = prompt("Please enter how long you want your password to be : Must be 8-128");
    }
  var lowercaseChar = confirm("Want lowercase character?");
-    if(true) {
+    if(lowercaseChar) {
       alert("You have selected lowercase letters.")
     }
     else {
       alert("You've declined a lowercase letters")
     }
   var uppercaseChar = confirm("want uppercase?");
-   if(true) {
-     alert("You've selected uppercase letter")    
+   if(uppercaseChar) {
+    //call a function here 
+    alert("You've selected uppercase letter")    
    }
   else {
     alert("You've declined uppercase letters")
   }
 var numberChar = confirm("Want numbers?");
-  if(true) {
+  if(numberChar) {
     alert("You've selected numbers")
   }
 else {
-  alert("You've declined nuumbers")
+  alert("You've declined numbers")
 }
 var specialChar = confirm("Want special characters");
-  if(true) {
+  if(specialChar) {
     alert("You've selcted special characters")
   }
   else {
@@ -51,7 +64,8 @@ var specialChar = confirm("Want special characters");
    passwordText.value = password;
  
  }
- 
+ //four loop array 
+
 
  // Add event listener to generate button
 
