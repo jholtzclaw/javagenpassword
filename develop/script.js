6dd9b11448eb655e08eb9a8aea92e01b4dec3065
Jena Holtzclaw <65194896+jholtzclaw@users.noreply.github.com>
2020-06-26 16:36:40 -0500

    working on function return password

--- develop/script.js
+++ develop/script.js
@@ -1,72 +1,123 @@
- // Get references to the #generate element
- var generateBtn = document.querySelector("#generate");
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 
-var passLength;
+var passwordLength;
+var lowercaseChar
+var uppercaseChar
+var numberChar
+var specialChar
 
 
- // Arrays for letter, special characters, and numbers.
- var lowercaseChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' ];
- var uppercaseChar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' ];
- var numberChar = ['1','2','3','4','5','6','7','8','9','0' ];
- var specialChar= ['!','@','#','$','%','^','&','*' ];
- 
+// Arrays for letter, special characters, and numbers.
+var lowercaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppercaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numberChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*'];
 
- // Variable Declaration
- var confirmLowercaseChar;
- var confirmUppercaseChar;
- var confirmNumberChar;
- var specialChar;
 
- // Get references to the #generate element
- var generateBtn = document.querySelector("#generate");
- 
- // Write password to the #password input
- function writePassword() {
-  
-   // var password = generatePassword();
-   var passwordLength = prompt("Please enter how long you want your password to be : Must be 8-128");
-   while(passwordLength < 8 || passwordLength > 128) {
-     alert("Incorrect length");
-     var passwordLength = prompt("Please enter how long you want your password to be : Must be 8-128");
-   }
- var lowercaseChar = confirm("Want lowercase character?");
-    if(lowercaseChar) {
-      alert("You have selected lowercase letters.")
-    }
-    else {
-      alert("You've declined a lowercase letters")
-    }
-  var uppercaseChar = confirm("want uppercase?");
-   if(uppercaseChar) {
-    //call a function here 
-    alert("You've selected uppercase letter")    
-   }
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+
+  // var password = generatePassword();
+  passwordLength = prompt("Please enter how long you want your password to be : Must be 8-128");
+  while (passwordLength < 8 || passwordLength > 128) {
+    alert("Incorrect length");
+    passwordLength = prompt("Please enter how long you want your password to be : Must be 8-128");
+  }
+  lowercaseChar = confirm("Want lowercase character?");
+  if (lowercaseChar) {
+    alert("You have selected lowercase letters.")
+  }
+  else {
+    alert("You've declined a lowercase letters")
+  }
+  uppercaseChar = confirm("want uppercase?");
+  if (uppercaseChar) {
+    alert("You've selected uppercase letter")
+  }
   else {
     alert("You've declined uppercase letters")
   }
-var numberChar = confirm("Want numbers?");
-  if(numberChar) {
+  numberChar = confirm("Want numbers?");
+  if (numberChar) {
     alert("You've selected numbers")
   }
-else {
-  alert("You've declined numbers")
-}
-var specialChar = confirm("Want special characters");
-  if(specialChar) {
+  else {
+    alert("You've declined numbers")
+  }
+  specialChar = confirm("Want special characters");
+  if (specialChar) {
     alert("You've selcted special characters")
+
   }
   else {
     alert("You've declined special characters")
+
   }
+  generatePassword()
  
+
+}
+//for loop array 
+function generatePassword() {
+  var passwordOptions = {
+    length: passwordLength,
+    hasLowercase: lowercaseChar,
+    hasUppercase: uppercaseChar,
+    hasNumbers: numberChar,
+    hasSpecial: specialChar
+  }
+
+  var result = []
+  var possibleChar = []
+  var guaranteedChar = []
+
+  if (passwordOptions.hasLowercase) {
+    possibleChar = possibleChar.concat(lowercaseChar);
+    guaranteedChar.push(getRandom(lowercaseChar))
+  }
+
+  if (passwordOptions.hasUppercase) {
+    possibleChar = possibleChar.concat(uppercaseChar);
+    guaranteedChar.push(getRandom(uppercaseChar))
+  }
+
+  if (passwordOptions.hasNumbers) {
+    possibleChar = possibleChar.concat(numberChar);
+    guaranteedChar.push(getRandom(numberChar))
+  }
+
+  if (passwordOptions.hasSpecial) {
+    possibleChar = possibleChar.concat(specialChar);
+    guaranteedChar.push(getRandom(specialChar))
+  }
+
+  for(var i = 0; i < passwordOptions.length; i++){
+    var possibleCharacter = getRandom(possibleChar)
+    result.push(possibleCharacter)
+  }
+
+  for(var i = 0; i < guaranteedChar.length; i++){
+    result[i] = guaranteedChar[i]
+  }
+
+  
    var passwordText = document.querySelector("#password");
- 
-   passwordText.value = password;
- 
- }
- //four loop array 
 
+   passwordText.value = result.join("");
+}
 
- // Add event listener to generate button
+function getRandom(arr) {
+  var ranIndex = Math.floor(Math.random() * arr.length)
+  var ranElement = arr[ranIndex]
+  return ranElement
+}
 
- generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button
+
+generateBtn.addEventListener("click", writePassword);
